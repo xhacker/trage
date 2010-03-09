16dd8beee8f0b9ab44aa454cf1460a6ed6e96992
Xhacker Liu <liu.dongyuan@gmail.com>
2010-03-09 17:00:12 +0800

    不可用版本。 重构了整个系统。

---
+++ judge_common.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Judge:
+    def __init__(self, pack, id, source_file):
+        '''读取题目配置文件'''
+        pass
+
+    def compile(self):
+        '''编译'''
+        pass
+
+    def judge(self):
+        '''评测一个测试点'''
+        pass
+    
---
+++ prob_common.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Problem:
+    def __init__(self, pack, id):
+        '''读取题目配置文件及数据库'''
+        pass
+
+    def getName(self):
+        '''TODO'''
+        pass
+
+    def getInfo(self):
+        '''TODO'''
+        pass
