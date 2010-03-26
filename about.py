eacecd2e29020b8882d34d5459f6a4bbf880422f
Xhacker Liu <liu.dongyuan@gmail.com>
2010-03-26 09:25:34 +0800

    增加 GUI，已经基本可用。

---
+++ about.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+import gtk
+
+class AboutFooDialog(gtk.AboutDialog):
+    __gtype_name__ = "AboutTrageDialog"
+
+    def __init__(self):
+        pass
+
+    def finish_init(self, builder):
+        self.builder = builder
+        self.builder.connect_signals(self)
+
+        #code for other initialization actions should be added here
+
+def new_about():
+    ui_filename = 'ui/about.glade'
+    builder = gtk.Builder()
+    builder.add_from_file(ui_filename)    
+    dialog = builder.get_object("about_trage_dialog")
+    dialog.finish_init(builder)
+    return dialog
+
+if __name__ == "__main__":
+    dialog = NewAboutFooDialog()
+    dialog.show()
+    gtk.main()
+
